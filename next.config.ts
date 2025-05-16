daa77f97057a99bcb16f9b6f1a2dbbe8c5c4af6b
rl627 <rl627@drexel.edu>
2025-05-16 12:55:33 -0400

    udpate base path

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/test-next-gh-pages",
 
   /**
    * Disable server-based image optimization. Next.js does not support
