63854ccd1c0ae95c8d9e356ff1374238c91f2636
rl627 <rl627@drexel.edu>
2025-05-16 14:01:47 -0400

    test adding shad components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ShadButton } from "@/components/ShadButton";
 
 export default function Home() {
   return (
@@ -48,6 +49,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        <div>Hi</div>
+        <div> <ShadButton/> </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
